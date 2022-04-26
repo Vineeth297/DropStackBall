d973271955050328a54e707f72553eb8c0199864
Vineeth <mamidipallyvineeth@gmail.com>
2022-04-26 18:55:10 +0530

    Naah!

--- Assets/ChessGame/Scripts/PieceMoverScript.cs
+++ Assets/ChessGame/Scripts/PieceMoverScript.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Timeline;
 using UnityEngine;
 
 public class PieceMoverScript : MonoBehaviour
@@ -39,14 +37,17 @@ public class PieceMoverScript : MonoBehaviour
 					//SoldierFunction(finalTransform);
 					RookFunction(finalTransform);
 				}
-				if(!pieceSelected)
+
+				if (!pieceSelected)
+				{
 					if (hit.collider.CompareTag("Piece"))
 					{
 						selectedTransform = hit.collider.gameObject.transform;
 						pieceSelected = !pieceSelected;
 						//check if its valid move ??
-						
+
 					}
+				}
 			}
 		}
 	}
@@ -67,22 +68,7 @@ public class PieceMoverScript : MonoBehaviour
 				 Array.IndexOf(checkBoxPositions, finalMove.position) == killerMoveIndex2)
 		{
 			//if the positions are occupied by enemy => kill
-			if (finalMove.childCount == 2)
-			{
-				//if the target piece is not of same color => kill
-				if (selectedTransform.GetComponent<Soldier>().isWhite !=
-					finalTransform.GetChild(1).GetComponent<Soldier>().isWhite)
-				{
-					selectedTransform.position = finalMove.GetChild(0).position;
-					pieceSelected = !pieceSelected;
-					selectedTransform.parent = finalMove;	
-				}
-				else
-					InvalidMove();
-			}
-			//else invalid move
-			else
-				InvalidMove();
+			KillTheEnemy(finalMove);
 		}
 		else
 			InvalidMove();
@@ -108,7 +94,8 @@ public class PieceMoverScript : MonoBehaviour
 		var currentIndex = Array.IndexOf(checkBoxPositions, selectedTransform.parent.position);
 		var finalPositionIndex = Array.IndexOf(checkBoxPositions, finalMove.position);
 		var remainder = (Mathf.Abs(currentIndex - finalPositionIndex)) % 8;
-
+		
+		//Vertical Switch
 		if (remainder == 0)
 		{
 			var maxIterationNumber = Mathf.Abs(currentIndex - finalPositionIndex) / 8;
@@ -147,14 +134,20 @@ public class PieceMoverScript : MonoBehaviour
 
 			if(numberOfEmptyBoxes == maxIterationNumber)
 			{
+				//if there is enemy on final Check box => kill
 				ValidateOccupancyAndMove(finalMove);
 				print("CanMove");
 			}
 		}
+		//Horizontal Switch
 		else if(MathF.Abs(currentIndex - finalPositionIndex) < 8)
 		{
 			var maxIterationNumber = Mathf.Abs(currentIndex - finalPositionIndex);
+			var lowestAllowedIndex = currentIndex - 8;
+			var hightestAllowedIndex = currentIndex + lowestAllowedIndex + 1;
 			var numberOfEmptyBoxes = 0;
+			
+			RookInvalidCases();
 			
 			for (var i = 1; i <= maxIterationNumber; i++)
 			{
@@ -195,8 +188,6 @@ public class PieceMoverScript : MonoBehaviour
 			}
 			InvalidMove();
 		}
-
-		//final position - current position == abs(1)
 	}
 
 	private void ValidateOccupancyAndMove(Transform finalMove)
@@ -219,5 +210,85 @@ public class PieceMoverScript : MonoBehaviour
 		selectedTransform = null;
 		finalTransform = null;
 	}
-	
+
+	private void KillTheEnemy(Transform finalMove)
+	{
+		if (finalMove.childCount == 2)
+		{
+			//if the target piece is not of same color => kill
+			if (selectedTransform.GetComponent<Soldier>().isWhite !=
+				finalTransform.GetChild(1).GetComponent<Soldier>().isWhite)
+			{
+				selectedTransform.position = finalMove.GetChild(0).position;
+				pieceSelected = !pieceSelected;
+				selectedTransform.parent = finalMove;	
+			}
+			else
+				InvalidMove();
+		}
+		//else invalid move
+		else
+			InvalidMove();
+	}
+
+	private void RookInvalidCases()
+	{
+		var currentIndex = Array.IndexOf(checkBoxPositions, selectedTransform.parent.position);
+		var finalPositionIndex = Array.IndexOf(checkBoxPositions, finalTransform.position);
+
+		/*
+		if (Array.IndexOf(checkBoxPositions, finalTransform.position) != (currentIndex + 7) ||
+			Array.IndexOf(checkBoxPositions, finalTransform.position) != (currentIndex + 9) ||
+			Array.IndexOf(checkBoxPositions, finalTransform.position) != (currentIndex - 7) ||
+			Array.IndexOf(checkBoxPositions, finalTransform.position) != (currentIndex - 9))
+		{
+			
+		}*/
+
+		var minIndex = 0;
+		var maxIndex = 0;
+
+		if (currentIndex >= 0 && currentIndex < 8)
+		{
+			minIndex = 0;
+			maxIndex = 7;
+		}
+		if (currentIndex >= 8 && currentIndex < 16)
+		{
+			minIndex = 8;
+			maxIndex = 15;
+		}
+		if (currentIndex >= 16 && currentIndex < 24)
+		{
+			minIndex = 16;
+			maxIndex = 23;
+		}
+		if (currentIndex >= 24 && currentIndex < 32)
+		{
+			minIndex = 24;
+			maxIndex = 31;
+		}
+		if (currentIndex >= 32 && currentIndex < 40)
+		{
+			minIndex = 32;
+			maxIndex = 39;
+		}
+		if (currentIndex >= 40 && currentIndex < 48)
+		{
+			minIndex = 40;
+			maxIndex = 47;
+		}
+		if (currentIndex >= 48 && currentIndex < 56)
+		{
+			minIndex = 48;
+			maxIndex = 56;
+		}
+		if (currentIndex >= 56 && currentIndex < 64)
+		{
+			minIndex = 56;
+			maxIndex = 63;
+		}
+		
+		
+	}
 }
